a77f8b3d9b40f73d64ea3dbed34042f793859ef4
Aecio Lucio <aecio.filho@redeig.com>
2025-02-27 17:39:41 -0400

    adiciona script para sortear arquivos Python em um diretório

---
+++ topicos/sorteio/Sorteio_aula_para_revisar.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import random
+
+def listar_arquivos(caminho_raiz):
+    return [str(arquivo) for arquivo in Path(caminho_raiz).rglob('*.py') if arquivo.is_file()]
+
+def sortear_arquivo(caminho_raiz):
+    arquivos = listar_arquivos(caminho_raiz)
+    return random.choice(arquivos) if arquivos else None  # Retorna None se não houver arquivos
+
+print(sortear_arquivo('.'))
+
+
